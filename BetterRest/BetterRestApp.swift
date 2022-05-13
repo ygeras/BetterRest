930bf39cbd24912d9041e61e649552911692c968
ygeras <86482316+Yus312@users.noreply.github.com>
2022-05-13 09:40:43 +0600

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Yuri Gerasimchuk on 13.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
